91b675dd821ba0c5c8c36a7a78ef9f344190a07e
Jason Anariba <Jassongomz11@gmail.com>
2023-10-12 23:39:03 -0600

    Review-3

--- index.js
+++ index.js
@@ -1,54 +1,60 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express();
 const port = 3005;
-const listViewRouter = require('./list-view-router');
-const listEditRouter = require('./list-edit-router');
 
-// Middleware para validar métodos HTTP
-function validateHTTPMethods(req, res, next) {
-  if (req.method !== 'GET' && req.method !== 'POST' && req.method !== 'PUT' && req.method !== 'DELETE') {
-    return res.status(405).send("Método HTTP no permitido");
+// Middleware para parsear JSON
+app.use(express.json());
+
+// Usuarios predefinidos (simulación)
+const users = [
+  { id: 1, username: 'user1', password: 'password1' },
+  { id: 2, username: 'user2', password: 'password2' },
+];
+
+// Ruta de login
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Simulación de autenticación
+  const user = users.find(u => u.username === username && u.password === password);
+
+  if (!user) {
+    return res.status(401).json({ error: 'Credenciales inválidas' });
   }
-  next();
+
+  // Crear token JWT
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+  res.json({ token });
+});
+
+// Middleware para validar token JWT
+function validateToken(req, res, next) {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ error: 'Token no proporcionado' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: 'Token no válido' });
+  }
 }
 
-// Middleware para validar parámetros en list-view-router
-function validateParams(req, res, next) {
-  const taskId = req.params.taskId;
-  if (!taskId || !/^\d+$/.test(taskId)) {
-    return res.status(400).json({ error: "Parámetro taskId no válido" });
-  }
-  next();
-}
+// Ruta protegida
+app.get('/protected', validateToken, (req, res) => {
+  res.json({ message: 'Ruta protegida alcanzada', user: req.user });
+});
 
-// Middleware para manejar solicitudes POST con el cuerpo vacío o datos inválidos
-function validateCreateTask(req, res, next) {
-  const newTask = req.body;
-  if (!newTask || !newTask.description) {
-    return res.status(400).json({ error: "Datos de tarea no válidos" });
-  }
-  next();
-}
-
-// Middleware para manejar solicitudes PUT con el cuerpo vacío o datos inválidos
-function validateUpdateTask(req, res, next) {
-  const updatedTask = req.body;
-  if (!updatedTask || !updatedTask.description) {
-    return res.status(400).json({ error: "Datos de tarea no válidos" });
-  }
-  next();
-}
-
-// Agrega el middleware para validar métodos HTTP a nivel de aplicación
-app.use(validateHTTPMethods);
-
-// Montar el router de lista de visualización en '/list-view'
-app.use('/list-view', listViewRouter);
-
-// Montar el router de edición de lista en '/list-edit'
-app.use('/list-edit', listEditRouter);
-
-// ..
+// ...
 
 app.listen(port, () => {
   console.log(`Servidor Express en funcionamiento en el puerto ${port}`);
---
+++ review-3.md
@@ -0,0 +1,36 @@
+Producto: Aplicación de Gestión de Tareas con Autenticación
+Propósito:
+La aplicación tiene como objetivo proporcionar a los usuarios una plataforma para gestionar sus tareas de manera segura y eficiente. Además, se ha implementado un sistema de autenticación mediante tokens JWT para garantizar la privacidad y seguridad de los usuarios.
+
+
+
+uncionalidades Principales:
+
+Autenticación de Usuarios:
+
+Importancia: Para garantizar la seguridad, cada usuario puede autenticarse mediante un sistema de tokens JWT.
+Razón de Uso: Los usuarios usarán esta funcionalidad para acceder a sus datos de tareas de manera segura y personalizada.
+Creación de Tareas:
+
+Importancia: Permite a los usuarios agregar nuevas tareas a su lista personal.
+Razón de Uso: Los usuarios usarían esto para registrar nuevas actividades o recordatorios en su aplicación.
+Eliminación de Tareas:
+
+Importancia: Los usuarios pueden eliminar tareas que ya no son necesarias.
+Razón de Uso: Para mantener su lista de tareas limpia y enfocarse en las actividades más relevantes.
+Actualización de Tareas:
+
+Importancia: Permite a los usuarios modificar detalles de una tarea, como su descripción o estado de finalización.
+Razón de Uso: Para reflejar cambios en las tareas existentes y mantener la información actualizada.
+Listar Todas las Tareas:
+
+Importancia: Proporciona una visión general de todas las tareas del usuario.
+Razón de Uso: Los usuarios usarán esta funcionalidad para ver todas sus tareas de manera consolidada.
+Listar Tareas Completas e Incompletas:
+
+Importancia: Permite a los usuarios filtrar tareas según su estado de finalización.
+Razón de Uso: Para evaluar rápidamente las tareas completadas y las pendientes.
+Obtener una Sola Tarea:
+
+Importancia: Proporciona detalles específicos de una tarea.
+Razón de Uso: Los usuarios podrían necesitar información detallada sobre una tarea en particular.
